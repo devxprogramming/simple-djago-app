b6f89b6d9b19ee4619fd09a20395c7de1086b68e
Sylvester <devxprogramming@gmail.com>
2024-10-20 14:30:04 -0400

    Updated requirements file

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-qrl-code.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
